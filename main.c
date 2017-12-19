6696a30b2c181f376dd00d5c3882573a8f282017
stvngrcia <27@holbertonschool.com>
2017-12-19 15:45:17 -0800

    adding call to extern variable and printing first node

--- main.c
+++ main.c
@@ -8,10 +8,12 @@
  */
 int main(int argc, char **argv)
 {
-	(void)argv;
 
+	extern stack_t *head;
+	head = NULL;
 	if (argc < 2 || argc > 2)
 		printf("call error(1)not file name or more than one file\n");
 	open_file(argv[1]);
+	printf("head->n %d\n", head->n);
 	return (0);
 }
