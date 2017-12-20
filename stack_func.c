23bf622bfa1651a28c821e199c6127aeb1ef9466
stvngrcia <stevengarciaacuna@gmail.com>
2017-12-20 11:06:26 -0800

    adding function to print the first node

--- stack_func.c
+++ stack_func.c
@@ -1,15 +1,25 @@
 #include "monty.h"
 
 /**
- * add_to_stack - Adds a node to the stack.
- * @new_node: Pointer to the new node.
+ * print_stack - Adds a node to the stack.
+ * @stack: Pointer to a pointer pointing to top node of the stack.
  * @line_number: Interger representing the line number of of the opcode.
  */
-void add_to_stack(stack_t **new_node, unsigned int line_number)
+void print_top(stack_t **stack, unsigned int line_number)
+{
+	if (stack == NULL || *stack == NULL)
+		err(6, line_number);
+	printf("%d\n", (*stack)->n);
+}
+/**
+ * add_to_stack - Adds a node to the stack.
+ * @new_node: Pointer to the new node.
+ * @ln: Interger representing the line number of of the opcode.
+ */
+void add_to_stack(stack_t **new_node, __attribute__((unused))unsigned int ln)
 {
 	stack_t *tmp;
 
-	(void) line_number;
 	if (new_node == NULL || *new_node == NULL)
 		exit(EXIT_FAILURE);
 	if (head == NULL)
