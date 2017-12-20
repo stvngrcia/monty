bd40bd56a804a59b4e192b5883585a2b09f0eb43
stvngrcia <stevengarciaacuna@gmail.com>
2017-12-20 12:03:42 -0800

    adding function that does nothing

---
+++ stack_func_2.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+
+/**
+ * nop - Does nothing.
+ * @stack: Pointer to a pointer pointing to top node of the stack.
+ * @line_number: Interger representing the line number of of the opcode.
+ */
+void nop(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
