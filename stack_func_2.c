f614836879f3085d2d5cc9d8cf53da55020c9348
stvngrcia <stevengarciaacuna@gmail.com>
2017-12-20 14:14:08 -0800

    adding implementation of add top nodes

--- stack_func_2.c
+++ stack_func_2.c
@@ -31,3 +31,22 @@ void swap_nodes(stack_t **stack, unsigned int line_number)
 	tmp->prev = NULL;
 	*stack = tmp;
 }
+
+/**
+ * add_nodes - Adds the top two elements of the stack.
+ * @stack: Pointer to a pointer pointing to top node of the stack.
+ * @line_number: Interger representing the line number of of the opcode.
+ */
+void add_nodes(stack_t **stack, unsigned int line_number)
+{
+	int sum;
+
+	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
+		err(8, line_number, "add");
+
+	(*stack) = (*stack)->next;
+	sum = (*stack)->n + (*stack)->prev->n;
+	(*stack)->n = sum;
+	free((*stack)->prev);
+	(*stack)->prev = NULL;
+}
