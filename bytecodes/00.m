43ef073ff5eb8fa116713c0ddd07cc4a6adb0f7d
stvngrcia <stevengarciaacuna@gmail.com>
2017-12-20 14:58:32 -0800

    fixing error messages. adding a break so that not all the cases print

--- bytecodes/00.m
+++ bytecodes/00.m
@@ -1,4 +1,4 @@
-push 0
+push
 push 1
 push 2
 push 3
--- errors.c
+++ errors.c
@@ -14,37 +14,42 @@
  */
 void err(int error_code, ...)
 {
-	va_list ag;
-	char *op;
-	int l_num;
+	va_list ag, char *op, int l_num;
 
 	va_start(ag, error_code);
-
 	switch (error_code)
 	{
 		case 1:
 			printf("USAGE: monty file\n");
+			break;
 		case 2:
 			printf("Error: Can't open file %s\n",
 				va_arg(ag, char *));
+			break;
 		case 3:
 			l_num = va_arg(ag, int);
 			op = va_arg(ag, char *);
 			printf("L%d: unknown instruction %s\n", l_num, op);
+			break;
 		case 4:
 			printf("Error: malloc failed\n");
+			break;
 		case 5:
 			printf("L%d: usage: push integer\n", va_arg(ag, int));
+			break;
 		case 6:
 			printf("L%d: can't pint, stack empty\n",
 				va_arg(ag, int));
+			break;
 		case 7:
 			printf("L%d: can't pop an empty stack\n",
 				va_arg(ag, int));
+			break;
 		case 8:
 			l_num = va_arg(ag, int);
 			op = va_arg(ag, char *);
 			printf("L%d: can't %s, stack too short\n", l_num, op);
+			break;
 		default:
 			break;
 	}
