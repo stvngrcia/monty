cb58bccdaa326bf517b5d2fc6fe5d047ca583fc5
stvngrcia <stevengarciaacuna@gmail.com>
2017-12-20 14:13:30 -0800

    correcting documentation

--- errors.c
+++ errors.c
@@ -8,8 +8,9 @@
  * (3) ~> The file provided contains an invalid instruction.
  * (4) ~> When the program is unable to malloc more memory.
  * (5) ~> When the parameter passed to the instruction "push" is not an int.
- * (6) ~> When the stack it empty.
- * (7) ~> When stack is too short for operation.
+ * (6) ~> When the stack it empty for pint.
+ * (7) ~> When the stack it empty for pop.
+ * (8) ~> When stack is too short for operation.
  */
 void err(int error_code, ...)
 {
@@ -23,35 +24,27 @@ void err(int error_code, ...)
 	{
 		case 1:
 			printf("USAGE: monty file\n");
-			break;
 		case 2:
 			printf("Error: Can't open file %s\n",
 				va_arg(ag, char *));
-			break;
 		case 3:
 			l_num = va_arg(ag, int);
 			op = va_arg(ag, char *);
 			printf("L%d: unknown instruction %s\n", l_num, op);
-			break;
 		case 4:
 			printf("Error: malloc failed\n");
-			break;
 		case 5:
 			printf("L%d: usage: push integer\n", va_arg(ag, int));
-			break;
 		case 6:
 			printf("L%d: can't pint, stack empty\n",
 				va_arg(ag, int));
-			break;
 		case 7:
 			printf("L%d: can't pop an empty stack\n",
 				va_arg(ag, int));
-			break;
 		case 8:
 			l_num = va_arg(ag, int);
 			op = va_arg(ag, char *);
 			printf("L%d: can't %s, stack too short\n", l_num, op);
-			break;
 		default:
 			break;
 	}
