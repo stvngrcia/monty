d7c0b3f0e70b19860a29bcaca09ed2aa6bf597ac
stvngrcia <stevengarciaacuna@gmail.com>
2017-12-21 13:59:59 -0800

    removing extra line

--- stack_func_2.c
+++ stack_func_2.c
@@ -85,9 +85,7 @@ void div_nodes(stack_t **stack, unsigned int line_number)
 	int sum;
 
 	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
-
 		more_err(8, line_number, "div");
-
 
 	if ((*stack)->n == 0)
 		more_err(9, line_number);
