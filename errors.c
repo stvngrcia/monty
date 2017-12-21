f7eaedfb7e58053bfef59864ec17b89b3a701a96
stvngrcia <stevengarciaacuna@gmail.com>
2017-12-21 09:30:48 -0800

    adding error handling funcitons

--- errors.c
+++ errors.c
@@ -86,3 +86,31 @@ void more_err(int error_code, ...)
 	free_nodes();
 	exit(EXIT_FAILURE);
 }
+
+/**
+ * string_err - handles errors.
+ * @error_code: The error codes are the following:
+ * (10) ~> The number inside a node is outside ASCII bounds.
+ * (11) ~> The stack is empty.
+ */
+void string_err(int error_code, ...)
+{
+	va_list ag;
+	int l_num;
+
+	va_start(ag, error_code);
+	l_num = va_arg(ag, int);
+	switch (error_code)
+	{
+		case 10:
+			printf("L%d: can't pchar, value out of range\n", l_num);
+			break;
+		case 11:
+			printf("L%d: can't pchar, stack empty\n", l_num);
+			break;
+		default:
+			break;
+	}
+	free_nodes();
+	exit(EXIT_FAILURE);
+}
