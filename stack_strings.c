b360ec3d76cf5ed5f0ff0efe28360580066b1998
stvngrcia <stevengarciaacuna@gmail.com>
2017-12-21 10:25:32 -0800

    adding implementation of rotl

--- stack_strings.c
+++ stack_strings.c
@@ -45,3 +45,26 @@ void print_str(stack_t **stack, __attribute__((unused))unsigned int ln)
 	}
 	printf("\n");
 }
+
+/**
+ * rotl - Rotates the first node of the stack to the bottom.
+ * @stack: Pointer to a pointer pointing to top node of the stack.
+ * @ln: Interger representing the line number of of the opcode.
+ */
+void rotl(stack_t **stack, __attribute__((unused))unsigned int ln)
+{
+	stack_t *tmp;
+
+	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
+		return;
+
+	tmp = *stack;
+	while (tmp->next != NULL)
+		tmp = tmp->next;
+
+	tmp->next = *stack;
+	(*stack)->prev = tmp;
+	*stack = (*stack)->next;
+	(*stack)->prev->next = NULL;
+	(*stack)->prev = NULL;
+}
