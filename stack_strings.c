ad90968a136f3ed9c42314cd4ba261f8ea475284
stvngrcia <stevengarciaacuna@gmail.com>
2017-12-21 09:32:16 -0800

    adding print_char implementation

---
+++ stack_strings.c
@@ -0,0 +1,19 @@
+#include "monty.h"
+
+/**
+ * print_char - Adds a node to the stack.
+ * @stack: Pointer to a pointer pointing to top node of the stack.
+ * @line_number: Interger representing the line number of of the opcode.
+ */
+void print_char(stack_t **stack, unsigned int line_number)
+{
+	int ascii;
+
+	if (stack == NULL || *stack == NULL)
+		string_err(11, line_number);
+
+	ascii = (*stack)->n;
+	if (ascii < 0 || ascii > 127)
+		string_err(10, line_number);
+	printf("%c\n", ascii);
+}
