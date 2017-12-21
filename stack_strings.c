3f9a29715f09c35c31ef7c7fbeeac397926f20d4
stvngrcia <stevengarciaacuna@gmail.com>
2017-12-21 09:44:16 -0800

    adding print string from ascii values implementation

--- stack_strings.c
+++ stack_strings.c
@@ -1,7 +1,7 @@
 #include "monty.h"
 
 /**
- * print_char - Adds a node to the stack.
+ * print_char - Prints the Ascii value.
  * @stack: Pointer to a pointer pointing to top node of the stack.
  * @line_number: Interger representing the line number of of the opcode.
  */
@@ -16,4 +16,29 @@ void print_char(stack_t **stack, unsigned int line_number)
 	if (ascii < 0 || ascii > 127)
 		string_err(10, line_number);
 	printf("%c\n", ascii);
+}
+
+/**
+ * print_str - Prints a string.
+ * @stack: Pointer to a pointer pointing to top node of the stack.
+ * @ln: Interger representing the line number of of the opcode.
+ */
+void print_str(stack_t **stack, __attribute__((unused))unsigned int ln)
+{
+	int ascii;
+	stack_t *tmp;
+
+	if (stack == NULL || *stack == NULL)
+		printf("\n");
+
+	tmp = *stack;
+	while (tmp != NULL)
+	{
+		ascii = tmp->n;
+		if (ascii <= 0 || ascii > 127)
+			break;
+		printf("%c", ascii);
+		tmp = tmp->next;
+	}
+	printf("\n");
 }
